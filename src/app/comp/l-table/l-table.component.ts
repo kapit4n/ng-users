ec3c82aa20cd18c5fbae3b2ca564362ca8236ae6
Luis Arce <luis.arce22@gmail.com>
2020-05-01 23:09:49 -0400

    create a common table to display lists

--- src/app/comp/l-table/l-table.component.ts
+++ src/app/comp/l-table/l-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-l-table',
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./l-table.component.css']
 })
 export class LTableComponent implements OnInit {
+
+  @Input() displayedColumns: string[];
+  @Input() title: string;
+  @Input() basicPath;
+  @Input() columnsConfig: any[];
+  @Input() dataSource: any[];
+  
+  removeFunction = (id: number) => { }
 
   constructor() { }
 
